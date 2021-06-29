eaf33e986937302babc7a706609546bb378da833
phamhuynhhaiyen <phamhuynhhaiyen1107@gmail.com>
2021-06-29 10:41:12 +0700

    Commit

---
+++ De6_DES/De6_DES/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace De6_DES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnGiaiMa = new System.Windows.Forms.Button();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSaveFile);
+            this.panel1.Controls.Add(this.btnGiaiMa);
+            this.panel1.Controls.Add(this.btnMaHoa);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 703);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(308, 654);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(170, 37);
+            this.btnSaveFile.TabIndex = 5;
+            this.btnSaveFile.Text = "Lưu kết quả vào file";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // btnGiaiMa
+            // 
+            this.btnGiaiMa.Location = new System.Drawing.Point(308, 356);
+            this.btnGiaiMa.Name = "btnGiaiMa";
+            this.btnGiaiMa.Size = new System.Drawing.Size(170, 33);
+            this.btnGiaiMa.TabIndex = 4;
+            this.btnGiaiMa.Text = "Giải mã";
+            this.btnGiaiMa.UseVisualStyleBackColor = true;
+            this.btnGiaiMa.Click += new System.EventHandler(this.btnGiaiMa_Click);
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Location = new System.Drawing.Point(23, 356);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(171, 33);
+            this.btnMaHoa.TabIndex = 3;
+            this.btnMaHoa.Text = "Mã hóa";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.btnMaHoa_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtOutput);
+            this.groupBox3.Location = new System.Drawing.Point(23, 397);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(455, 251);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kết quả";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(6, 22);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(443, 223);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtKey);
+            this.groupBox2.Location = new System.Drawing.Point(23, 284);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(455, 61);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Khóa bảo vệ";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(7, 22);
+            this.txtKey.MaxLength = 16;
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(442, 30);
+            this.txtKey.TabIndex = 0;
+            this.txtKey.Text = "0123456789ABCDE1";
+            this.txtKey.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtKey_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.btnOpenFile);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(455, 264);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mã hóa văn bản và giải mã";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(6, 55);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(443, 203);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.Text = "";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(255, 20);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(82, 26);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Chọn tệp ";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập nội dung hoặc mở tệp văn bản:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 703);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnGiaiMa;
+        private System.Windows.Forms.Button btnMaHoa;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ De6_DES/De6_DES/Form1.cs
@@ -0,0 +1,173 @@
+﻿using MaHoaDES.DoiTuong;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace De6_DES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int MaHoaHayGiaiMa = 1;
+
+        DES64Bit MaHoaDES64;
+        Khoa Khoa;
+        //Thread thread;
+        public static string TenTienTrinh = "";
+        public static int GiaiDoan = -1;
+        private static int Dem = 0;
+        private void EnableHoacDisableNut(bool b)
+        {
+            btnOpenFile.Enabled = btnSaveFile.Enabled = btnGiaiMa.Enabled = btnMaHoa.Enabled  = b;
+        }
+        private void MaHoa()
+        {
+            MaHoaDES64 = new DES64Bit();
+            TenTienTrinh = "";
+            GiaiDoan = 0;
+            Dem = 0;
+            Khoa = new Khoa(txtKey.Text);
+            if (MaHoaHayGiaiMa == 1)
+            {
+
+                MaHoaDES64 = new DES64Bit();
+                GiaiDoan = 0;
+                GiaiDoan = 1;
+                string kq = MaHoaDES64.ThucHienDESText(Khoa, txtInput.Text, 1);
+                txtOutput.Text = kq;
+                GiaiDoan = 2;
+                GiaiDoan = 3;
+                MessageBox.Show("Mã hóa thành công");
+            }
+            else
+            {
+                MaHoaDES64 = new DES64Bit();
+                GiaiDoan = 0;
+                GiaiDoan = 1;
+                string kq = MaHoaDES64.ThucHienDESText(Khoa, txtInput.Text, -1);
+                txtOutput.Text = kq;
+                if (kq == "")
+                {
+                    return;
+                }
+                GiaiDoan = 2;
+                GiaiDoan = 3;
+                MessageBox.Show("Giải mã thành công");
+            }
+    }
+        private void btnMaHoa_Click(object sender, EventArgs e)
+        {
+            if(txtInput.Text != string.Empty)
+            {
+                if (txtKey.TextLength < 16)
+                {
+                    MessageBox.Show("Vui lòng nhập đủ 16 ký tự khoá");
+                }
+                else
+                {
+                    MaHoaHayGiaiMa = 1;
+                    EnableHoacDisableNut(false);
+                    MaHoa();
+                    EnableHoacDisableNut(true);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng thêm dữ liệu");
+            }
+
+            
+        }
+
+        
+
+        private void txtKey_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsControl(e.KeyChar) && !(ChuoiHexa.BoHexa.Contains(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnGiaiMa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtInput.Text != string.Empty)
+                {
+                    if (txtKey.TextLength < 16)
+                    {
+                        MessageBox.Show("Vui lòng nhập đủ 16 ký tự");
+                    }
+                    else
+                    {
+                        MaHoaHayGiaiMa = -1;
+                        EnableHoacDisableNut(false);
+                        MaHoa();
+                        EnableHoacDisableNut(true);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng thêm dữ liệu");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Không thể giải mã!");
+                EnableHoacDisableNut(true);
+            }
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Chọn file(txt) | *.txt;";
+                openFileDialog.Multiselect = false;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = openFileDialog.FileName;
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                    StreamReader sr = new StreamReader(fs);
+                    sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                    string str;
+                    //doc tat ca du lieu trong file luu vao str;
+                    str = sr.ReadToEnd();
+                    txtInput.Text = str;
+                    sr.Close();
+                    fs.Close();
+                }
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //Hiển thị thư mục này đầu tiên
+            saveFileDialog.InitialDirectory = @"E:\";
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtOutput.Text);
+                MessageBox.Show("Lưu file thành công!");
+            }
+                
+        }
+    }
+}
